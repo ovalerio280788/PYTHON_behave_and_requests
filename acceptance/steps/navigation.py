b2742afccdcf18d7425fcd60c913878f2672983e
ovalerio280788 <osvamo280788@gmail.com>
2018-05-30 00:52:23 -0600

    First commit

--- acceptance/steps/navigation.py
+++ acceptance/steps/navigation.py
@@ -11,29 +11,3 @@ def step_impl(context):
     context.driver = webdriver.Chrome()
     page = LoginPage(context.driver)
     context.driver.get(page.url)
-
-
-# @given('I am on the blog page')
-# def step_impl(context):
-#     context.driver = webdriver.Chrome()
-#     page = BlogPage(context.driver)
-#     context.driver.get(page.url)
-#
-#
-# @given('I am on the new post page')
-# def step_impl(context):
-#     context.driver = webdriver.Chrome()
-#     page = NewPostPage(context.driver)
-#     context.driver.get(page.url)
-#
-#
-# @then('I am on the blog page')
-# def step_impl(context):
-#     expected_url = BlogPage(context.driver).url
-#     assert context.driver.current_url == expected_url
-#
-#
-# @then('I am on the homepage')
-# def step_impl(context):
-#     expected_url = HomePage(context.driver).url
-#     assert context.driver.current_url == expected_url
